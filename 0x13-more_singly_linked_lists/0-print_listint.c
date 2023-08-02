ecad378324aa2f99093182abd6e029db3011c791
Michael <micotejei@gmail.com>
2023-08-02 02:47:30 -0700

    Corrections

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,15 +1,16 @@
 #include "lists.h"
 
-size_g print_listint (const listint_t *h)
+size_t print_listint(const listint_t *h)
 {
-	size_g num = 0;
+	size_t num = 0;
 
 	while (h)
 	{
-		printf ("%d\n", h->n);
+		printf("%d\n", h->n);
 		num++;
 		h = h->next;
 	}
 
 	return (num);
 }
+
